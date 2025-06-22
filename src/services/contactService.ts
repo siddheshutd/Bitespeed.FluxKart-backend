d1948537989bb1b4539b76d42f8849fe8eecab25
Siddhesh Patil <siddheshutd@gmail.com>
2025-06-22 19:33:07 +0100

    This commit contains refractoring of service layer code for IdentifyByEmailAndNumber

--- src/services/contactService.ts
+++ src/services/contactService.ts
@@ -31,12 +31,13 @@ export class ContactService {
       if(!contact) { //If no existing contact found, create a new primary contact
         const newContact = await this.createContact('primary', email);
         return {contact: mapToContactListResponse(newContact, [])};
+      } 
 
-      } else { //Else directly process the response as we don't need to create a new contact
-        const primaryContact = await this.resolvePrimaryContact(contact);
-        return this.processIdentifyResponse(primaryContact!);
-      }
-    } catch (error) {
+      //Else directly process the response as we don't need to create a new contact
+      const primaryContact = await this.resolvePrimaryContact(contact);
+      return this.processIdentifyResponse(primaryContact!);
+    } 
+    catch (error) {
       console.error('Error in ContactService.identifyContactByEmail:', error);
       throw error;
     }
@@ -49,11 +50,13 @@ export class ContactService {
       if (!contact) { //If no existing contact found, create a new primary contact
         const newContact = await this.createContact('primary', undefined, phoneNumber);
         return {contact: mapToContactListResponse(newContact, [])};
-      } else { //Else directly process the response as we don't need to create a new contact
-        const primaryContact = await this.resolvePrimaryContact(contact);
-        return this.processIdentifyResponse(primaryContact!);
       }
-    } catch (error) {
+
+      //Else directly process the response as we don't need to create a new contact
+      const primaryContact = await this.resolvePrimaryContact(contact);
+      return this.processIdentifyResponse(primaryContact!);
+    } 
+    catch (error) {
       console.error('Error in ContactService.identifyContactByPhoneNumber:', error);
       throw error;
     }
@@ -64,58 +67,28 @@ export class ContactService {
       const contacts = await this.contactDataAccess.getContactsByPhoneNumberOrEmail(phoneNumber, email);
 
       if(contacts.length === 0) {//If no existing contacts found, create a new primary contact
-        const newContact = await this.createContact('primary', email, phoneNumber);
-        return {
-          contact: mapToContactListResponse(newContact, [])
-        };
-      } else {
-        //Check if there is an exact match
-        const exactMatch = contacts.find(contact => 
-          contact.email === email && contact.phoneNumber === phoneNumber
-        );
-        if(exactMatch) {
-          const primaryContact = await this.resolvePrimaryContact(exactMatch);
-          return this.processIdentifyResponse(primaryContact!); 
-        }
-
-        //Find out how many primary contacts are there
-        const primaryContactIds = new Set<number>();
-        contacts.forEach(contact => {
-          if (contact.linkPrecedence === 'primary') {
-            primaryContactIds.add(contact.id);
-          } else {
-            primaryContactIds.add(contact.linkedId!);
-          }
-        });
-        
-        const primaryIds = Array.from(primaryContactIds);
-        if(primaryIds.length === 1) { //If there is only one primary contact, create a new secondary contact
-          const primaryContactId = primaryIds[0];
-          const newContact = await this.createContact('secondary', email, phoneNumber, primaryContactId);
-
-          let primaryContact: Contact | null | undefined = contacts.find(contact => contact.id === primaryContactId);
-          if(!primaryContact) {
-            primaryContact = await this.contactDataAccess.getContactById(primaryContactId);
-          }
-          return this.processIdentifyResponse(primaryContact!);
-        } 
-        else { //If there are multiple primary contacts, demote the newer primary contact to secondary and update it's children
-          const primaryContacts = await this.contactDataAccess.getContactsByIds(primaryIds);
-          const primaryContact = primaryContacts[0];
-          const newSecondaryContact = primaryContacts[1];
-
-          var updatedContact: UpdateContactData = {
-            linkedId: primaryContact.id,
-            linkPrecedence: 'secondary'
-          }
-          //Update the new secondary contact to link to the primary contact
-          await this.updateContact(newSecondaryContact.id, updatedContact);
-          //Update all the children of secondary contact
-          await this.contactDataAccess.updateContactsByLinkedId(newSecondaryContact.id, primaryContact.id);
-          return this.processIdentifyResponse(primaryContact);
-        }
+        return this.createNewPrimaryContact(email, phoneNumber);
       }
-    } catch (error) {
+
+      //Check if there is an exact match
+      const exactMatch = contacts.find(contact => contact.email === email && contact.phoneNumber === phoneNumber);
+      if(exactMatch) {
+        const primaryContact = await this.resolvePrimaryContact(exactMatch);
+        return this.processIdentifyResponse(primaryContact!); 
+      }
+
+      //Find out how many primary contacts are there
+      const primaryIds = this.extractPrimaryContactIds(contacts);
+
+      if(primaryIds.length === 1) { //If there is only one primary contact, create a new secondary contact
+        const primaryContactId = primaryIds[0];
+        return this.handleSinglePrimaryContact(contacts, primaryContactId, email, phoneNumber);
+      } 
+      else { //If there are multiple(2) primary contacts, demote the newer primary contact to secondary and update it's children
+        return this.handleMultiplePrimaryContacts(primaryIds);
+      }
+    } 
+    catch (error) {
       console.error('Error in ContactService.identifyContactByEmailAndPhoneNumber:', error);
       throw error;
     }
@@ -137,7 +110,54 @@ export class ContactService {
     return await this.contactDataAccess.getContactById(primaryContactId);
   }
 
-  async createContact(precedence: 'primary' | 'secondary',email?: string, phoneNumber?: string, linkedId?: number): Promise<Contact> {
+  private async createNewPrimaryContact(email: string, phoneNumber: string): Promise<ContactIdentifyResponse> {
+    const newContact = await this.createContact('primary', email, phoneNumber);
+    return {
+      contact: mapToContactListResponse(newContact, [])
+    };
+  }
+
+  private extractPrimaryContactIds(contacts: Contact[]): number[] {
+    const primaryContactIds = new Set<number>();
+    
+    contacts.forEach(contact => {
+      if (contact.linkPrecedence === 'primary') {
+        primaryContactIds.add(contact.id);
+      } else {
+        primaryContactIds.add(contact.linkedId!);
+      }
+    });
+    
+    return Array.from(primaryContactIds);
+  }
+
+  private async handleSinglePrimaryContact(contacts: Contact[], primaryContactId: number, email: string, phoneNumber: string): Promise<ContactIdentifyResponse> {
+    const newContact = await this.createContact('secondary', email, phoneNumber, primaryContactId);
+
+    let primaryContact: Contact | null | undefined = contacts.find(contact => contact.id === primaryContactId);
+    if(!primaryContact) {
+      primaryContact = await this.contactDataAccess.getContactById(primaryContactId);
+    }
+    return this.processIdentifyResponse(primaryContact!);
+  }
+  
+  private async handleMultiplePrimaryContacts(primaryIds: number[]): Promise<ContactIdentifyResponse> {
+    const primaryContacts = await this.contactDataAccess.getContactsByIds(primaryIds);
+    const primaryContact = primaryContacts[0];
+    const newSecondaryContact = primaryContacts[1];
+
+    var updatedContact: UpdateContactData = {
+      linkedId: primaryContact.id,
+      linkPrecedence: 'secondary'
+    }
+    //Update the new secondary contact to link to the primary contact
+    await this.updateContact(newSecondaryContact.id, updatedContact);
+    //Update all the children of secondary contact
+    await this.contactDataAccess.updateContactsByLinkedId(newSecondaryContact.id, primaryContact.id);
+    return this.processIdentifyResponse(primaryContact);
+  }
+
+  private async createContact(precedence: 'primary' | 'secondary',email?: string, phoneNumber?: string, linkedId?: number): Promise<Contact> {
     try {
       let contactData: CreateContactData = {
         email: email ? email : undefined,
@@ -153,7 +173,7 @@ export class ContactService {
     }
   }
 
-  async updateContact(id: number, data: UpdateContactData): Promise<Contact> {
+  private async updateContact(id: number, data: UpdateContactData): Promise<Contact> {
     try {
       const updatedContact = await this.contactDataAccess.updateContact(id, data);
       return updatedContact;
