b2311d7a39d72639e551bf7b8b49257125b39baa
Siddhesh Patil <siddheshutd@gmail.com>
2025-06-22 19:59:17 +0100

    This commit contains addition of zod for schema validation

--- src/controllers/contactController.ts
+++ src/controllers/contactController.ts
@@ -1,6 +1,7 @@
 import { ContactService } from "../services/contactService";
 import { Request, Response } from 'express';
-import { ContactIdentifyRequest, ContactIdentifyResponse } from "../services/contactService";
+import { ContactIdentifyResponse } from "../services/contactService";
+import { IdentifyRequestSchema } from "../request-schemas/identify.request-schema";
 
 export class ContactController {
     private contactService: ContactService;
@@ -8,9 +9,18 @@ export class ContactController {
     constructor() {
         this.contactService = new ContactService();
     }
-    async identify(req: Request, res: Response) {
+    async identify(req: Request, res: Response): Promise<void> {
         try {
-            const { email, phoneNumber }: ContactIdentifyRequest = req.body;
+            const validationResult = IdentifyRequestSchema.safeParse(req.body);
+
+            if(!validationResult.success) {
+                res.status(400).json({
+                    error: "Validation failed",
+                    details: validationResult.error.errors
+                });
+            }
+
+            const { email, phoneNumber } = validationResult.data!;
 
             let result: ContactIdentifyResponse;    
             if(email && phoneNumber) {
@@ -21,10 +31,10 @@ export class ContactController {
                 result = await this.contactService.identifyContactByPhoneNumber(phoneNumber!.toString());
             }
 
-            res.status(200).json(result);
+             res.status(200).json(result);
         } catch (error) {
             console.error('Error in ContactController.identify:', error);
-            res.status(500).json({ 
+             res.status(500).json({ 
                 error: 'Internal server error',
                 message: (error as Error).message
             });
